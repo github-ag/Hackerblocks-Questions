d48ad75342f9acd57fb9bf7aaa07bee8bac91dac
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-03-17 12:21:18 +0530

    Update MinStepsToOne.cpp

--- Dynamic Programming/MinStepsToOne.cpp
+++ Dynamic Programming/MinStepsToOne.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+int mi[100]={0};
 int ms(int n)
 {
+    if(mi[n]!=0)
+        return mi[n];
     if(n<=3)
     return 1;
     int subone = 1+ms(n-1);
     int divtwo = 1+ms(n/2);
     int divthree = 1+ms(n/3);
-    return min(subone,min(divtwo,divthree));
+    return mi[n]=min(subone,min(divtwo,divthree));
 
 }
 int main()
