01f11d033c5664a40bda7405270096553fad6bdc
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-03-17 12:39:27 +0530

    Update MinStepsToOne.cpp

--- Dynamic Programming/MinStepsToOne.cpp
+++ Dynamic Programming/MinStepsToOne.cpp
@@ -1,3 +1,4 @@
+//top down approach
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -5,7 +6,7 @@ int mi[100]={0};
 int ms(int n)
 {
     if(mi[n]!=0)
-        return mi[n];
+    return mi[n];
     if(n<=3)
     return 1;
     int subone = 1+ms(n-1);
@@ -19,5 +20,42 @@ int main()
     int n;
     cin>>n;
     cout<<ms(n);
+    return 0;
+}
+
+
+
+
+
+
+
+//bottom up approach
+#include<iostream>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int mi[100];
+    mi[0]=0;
+    mi[1]=0;
+   
+    for(int i=2;i<=n;i++)
+    {
+        mi[i] = 1+mi[i-1];
+        if(i%2==0)
+        {
+            mi[i] = min(1+mi[i/2],mi[i]);
+        }
+        if(i%3==0)
+        {
+            mi[i] = min(1+mi[i/3],mi[i]);
+        }
+        
+    }
+    // in the bottom up approach we cannot use three different variables as the for loop is running in the same function so variables already has the values.
+    cout<<mi[n];
     return 0;
 }
