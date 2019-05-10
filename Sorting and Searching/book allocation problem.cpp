d56b8242478456c4fb8af00eaf1277873913a62d
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-05-10 21:21:58 +0530

    Create book allocation problem.cpp

---
+++ Sorting and Searching/book allocation problem.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    int n;
+    int x;
+    cin>>t>>n>>x;
+    int a[100];
+    int sum=0;
+    while(t--){
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        sum+=a[i];
+
+    }
+    int e = sum;
+    int s = 0;
+    int mid = (s+e)/2;
+    int ans;
+    while(s<=e)
+    {
+        mid = (s+e)/2;
+        int students=1;
+        int pages=0;
+        for(int i=0;i<n;i++)
+        {
+
+            pages+=a[i];
+            if(pages>mid)
+            {
+                students++;
+                //pages = a[i];
+                i=i-1;
+                pages=0;
+
+            }
+            if(students>x)
+            {
+
+                s = mid+1;
+                break;
+            }
+        }
+        if(students<=x)
+        {
+            ans = mid;
+            e = mid-1;
+        }
+
+
+    }
+    cout<<ans;
+    }
+    return 0;
+
+}
